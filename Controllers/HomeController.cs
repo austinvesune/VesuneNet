5423990fa75ae01fa03716dad3e1d952f76a7cfc
illuseyours@icloud.com <illuseyours@icloud.com>
2020-05-03 20:10:58 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VesuneNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+        
+        public ActionResult LawnMowers()
+        {
+            ViewBag.Message = this.GetMyPhoneNumber();
+
+            return View();
+        }
+
+        public string GetMyPhoneNumber ()
+        {
+            string phoneNUmber = "9103820928";
+            return phoneNUmber;
+        }
+    }
+}
---
+++ Models/CarModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VesuneNet.Models
+{
+    public class CarModel
+    {
+        public string CarName { get; set; }
+        public int HOwManyWheels { get; set; }
+        public string color { get; set; }
+    }
+}
